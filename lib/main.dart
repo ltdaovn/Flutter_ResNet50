1538023298c7671e1af82b2d2032c0d23666ed93
ltdaovn <54632789+ltdaovn@users.noreply.github.com>
2021-07-23 10:53:20 +0700

    add

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'predict.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Classification-Catdog',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+
+    home: Predict()
+    );
+  }
+}
---
+++ lib/predict.dart
@@ -0,0 +1,127 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+class resp{
+  String? categories;
+
+  resp({this.categories});
+  resp.fromJson(Map<String,dynamic>json){
+    categories = json['categories'];
+  }
+
+  Map<String,dynamic> toJson(){
+    final Map<String,dynamic> data = new Map<String,dynamic>();
+    data['categories'] = this.categories;
+    return data;
+  }
+}
+
+class Predict extends StatefulWidget{
+  @override
+  _PredictState createState() => _PredictState();
+}
+
+class _PredictState extends State<Predict> {
+  late FToast fToast;
+  final picker = ImagePicker();
+  File? imgf;
+  String url = "http://class-catdog.herokuapp.com/image";
+  String pathimg = "";
+  String pathpre = "";
+  String final_response = "";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("Upload Image"),),
+      body: Container(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height:50),
+                imgf!=null?
+                new Image.file(imgf!): new Text("Select an image to upload"),
+                SizedBox(height:20),
+                RaisedButton(
+                  color:Colors.blue,
+                  textColor:Colors.white,
+                  child:Text("Pick Image from gallery",),
+                  onPressed: ()async
+                  {
+                    var pickedFile = await picker.getImage(source: ImageSource.gallery);
+                    final File fileimg = File(pickedFile!.path);
+                    pathimg = pickedFile.path;
+                    if(pickedFile != null){
+                      Fluttertoast.showToast(
+                          msg: "Upload Image Success",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.grey,
+                          textColor: Colors.red,
+                          fontSize: 10.0
+                      );
+                    }else{
+                      Fluttertoast.showToast(
+                          msg: "Fail To Upload Image",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.grey,
+                          textColor: Colors.red,
+                          fontSize: 10.0
+                      );
+                    }
+                    setState(() {
+                      imgf = fileimg;
+                      pathpre = pathimg;
+                    });
+
+                  },
+                ),
+                FlatButton(
+                  color:Colors.blue,
+                  textColor:Colors.white,
+                  child:Text("Predict",),
+                  onPressed: ()async
+                  {
+                    final request = http.MultipartRequest("POST", Uri.parse(url));
+                    final multipartFile = await http.MultipartFile.fromPath('file', pathpre,contentType: MediaType('image', 'jpeg'));
+                    request.files.add(multipartFile);
+                    final streamedResponse = await request.send();
+                    final response = await http.Response.fromStream(streamedResponse);
+                    if(response.statusCode == 200){
+                      Fluttertoast.showToast(
+                          msg: "Predict Success",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.grey,
+                          textColor: Colors.red,
+                          fontSize: 10.0
+                      );
+                    }
+                    final List responseData = json.decode(response.body) ;
+                    setState(() {
+                      final_response = responseData.toString().replaceAll(RegExp(r'[{[}]'), '').replaceAll(']','');
+                      final_response = final_response.replaceAll(',','\n').replaceAll("categories:", "");
+                    });
+                  },
+                ),
+                Expanded(
+                    child:
+                    Text(final_response, style: TextStyle(fontSize: 10,color: Colors.blue,fontWeight: FontWeight.bold),)),
+              ],),
+          )
+      ),);
+  }
+
+
+
+}
